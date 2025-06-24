040f778bfd82445641911468d047dcc71aef83e4
Sayyed Wasim Hussain Bukhari <wasimhussain3@gmail.com>
2025-06-24 22:05:52 +0200

    Changed FileStructure according to Mentor's Comments

--- app/about_us/page.js
+++ app/about_us/page.js
@@ -1,12 +1,12 @@
-//import styles from "./page.module.css";
-import OurValues from "./react1-week1/values/OurValues";
-import OurCrew from "./react1-week1/crew/OurCrew";
-import OurPartners from "./react1-week1/partners/OurPartners";
-
+// import styles from "./page.module.css";
 // TASK - React 1 week 1
 // After you are finished with creating the page, move the OurValues, OurCrew, OurPartners components into their own files
 // OurValues.js, OurCrew.js, OurPartners.js should live in this folder
 // import and use the components from the newly created files
+
+import OurCrew from "@/components/crew/OurCrew";
+import OurPartners from "@/components/partners/OurPartners";
+import OurValues from "@/components/values/OurValues";
 
 export const Crew = () => {
   return (
